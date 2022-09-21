41b0df2f8bb227541e3eb53562af17536bb206a7
karolh2000 <karolh@jfrog.com>
2022-09-21 07:53:33 -0700

    Don't clean up env to see if it improves the tests env creation.

--- bats/tests/common-setup.bash
+++ bats/tests/common-setup.bash
@@ -5,7 +5,7 @@ PYRSIA_TEMP_DIR=/tmp/pyrsia_tests/pyrsia
 # the pyrsia binaries
 PYRSIA_TARGET_DIR=$PYRSIA_TEMP_DIR/target/release
 # if "true" then the temp files (pyrsia sources, binaries, etc.) and the docker images/containers are destroyed in "teardown_file" method.
-CLEAN_UP_TEST_ENVIRONMENT=true
+CLEAN_UP_TEST_ENVIRONMENT=false
 
 _common_setup() {
   # load the bats "extensions"
